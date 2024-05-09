0b0f127b2433f0c14db48281bb6c5203fc2cd1b3
Emili-HD <emili@vilax.es>
2024-05-09 11:46:57 +0200

    añadida sección precio en cirugías

--- components/cirugias/Figure.vue
+++ components/cirugias/Figure.vue
@@ -12,7 +12,8 @@
             has-[img]:row-start-1 gap-8
             [.tabla_&]:col-[11/-1] [.tabla_&]:row-start-1 [.tabla_&]:w-full
             [.columnas_&]:col-[2/-2] [.columnas_&]:row-start-2 [.columnas_&]:p-0 [.columnas_&]:aspect-square [.columnas_&]:h-full
-            [.columnas_&]:lg:col-[6/12] [.columnas_&]:lg:row-start-1 [.columnas_&]:lg:px-16 [.columnas_&]:lg:aspect-[2/3]" 
+            [.columnas_&]:lg:col-[6/12] [.columnas_&]:lg:row-start-1 [.columnas_&]:lg:px-16 [.columnas_&]:lg:aspect-[2/3]
+            [#precio_&]:col-[2/-2] [#precio_&]:mb-8 [#precio_&]:lg:col-start-9 [#precio_&]:lg:mb-0" 
             v-if="contentData.fondo != 'sin'">
       <div v-if="contentData.fondo == 'vHorizontal'" class="video__player aspect-video">
          <div class="size-full">
@@ -27,7 +28,8 @@
       <NuxtImg class="rounded-xl size-full object-cover object-center" loading="lazy" v-else-if="contentData.fondo == 'imagen'" :src="contentData.side_image.url" :alt="contentData.side_image.alt" />
    </figure>
    <!-- </DelayHydration> -->
-   <div class="panel__content col-[2/-2] lg:col-[10_/_span_6] has-[.accordion]:col-[2_/_span_6] row-start-2 lg:row-start-1 [#precio_&]:col-start-2 [#precio_&]:col-end-8"
+   <div class="panel__content col-[2/-2] lg:col-[10_/_span_6] has-[.accordion]:col-[2_/_span_6] row-start-2 lg:row-start-1 [#precio_&]:col-start-2 
+               [#precio_&]:lg:col-end-8 [#precio_&]:flex [#precio_&]:flex-col [#precio_&]:justify-center"
       v-if="contentData && contentData.opciones_listado != 'columnas' && contentData.opciones_listado != 'antesdespues'">
       <h2 class="text-clamp-2xl mb-8">{{ contentData.heading }}</h2>
       <div class="answer p-0">
